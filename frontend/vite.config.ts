5967ca0d2b6b8167e0bc9799f39bb1f7832cba4a
flofriday <flohacksfriday@gmail.com>
2026-01-03 00:18:30 +0100

    Convert frontend build to single file build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,12 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/python-error-garden/',
+  plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
